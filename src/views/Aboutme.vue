24af2205142a97571076afe58ff50e61ecad30d1
Sadia Tasnim <s.sharna06@gmail.com>
2021-08-29 00:25:08 +0600

    experience updated

--- src/views/Aboutme.vue
+++ src/views/Aboutme.vue
@@ -1,97 +1,29 @@
 <template>
   <div class="card border-0">
-      <div class="card-header text-left darkblue-bg text-white">
-        {{title}}
-
-      </div>
-      <div class="card-body text-left font-14">
-        <p>
-            {{aboutmeDescription}}
-        </p>
-
-      </div>
+    <div class="card-header text-left darkblue-bg text-white">
+      {{ title }}
+    </div>
+    <div class="card-body text-left font-14">
+      <p>
+        {{ aboutmeDescription }}
+      </p>
+    </div>
   </div>
-
 </template>
 
 <script>
 export default {
-  name: 'Aboutme',
+  name: "Aboutme",
   props: {
-    title : String,
-    
+    title: String,
   },
   data() {
-        return {
-            jobStartmonth: 6,
-            jobStartyear: 2019,
-            jobDuration: '',
-            aboutmeDescription: '',
-        }
-    },
-    mounted() {
-        this.jobDuration = this.getExperienceDuration(this.jobStartyear, this.jobStartmonth);
-        this.aboutmeDescription = `I'm a software engineer from Bangladesh. I'm a true fan of competitive programming and web development.
-         Currently, I am working at Syntec Consulting Limited for ${this.jobDuration.years} yrs and ${this.jobDuration.months} mos which provides solution for Healthcare (EHR and EMR).`;
-
-    },
-    methods: {
-        getExperienceDuration(year, month) {
-
-
-            var now = new Date();
-            var today = new Date(now.getYear(), now.getMonth(), now.getDate());
-
-            var yearNow = now.getYear();
-            var monthNow = now.getMonth();
-            var dateNow = now.getDate();
-
-            var dob = new Date(year, month, now.getDate());
-
-            var yearDob = dob.getYear();
-            var monthDob = dob.getMonth();
-            var dateDob = dob.getDate();
-            var age = {};
-            var ageString = "";
-            var yearString = "";
-            var monthString = "";
-            var dayString = "";
-
-
-            var yearAge = yearNow - yearDob;
-
-            if (monthNow >= monthDob)
-                var monthAge = monthNow - monthDob;
-            else {
-                yearAge--;
-                var monthAge = 12 + monthNow - monthDob;
-            }
-
-            if (dateNow >= dateDob)
-                var dateAge = dateNow - dateDob;
-            else {
-                monthAge--;
-                var dateAge = 31 + dateNow - dateDob;
-
-                if (monthAge < 0) {
-                    monthAge = 11;
-                    yearAge--;
-                }
-            }
-
-            age = {
-                years: yearAge,
-                months: monthAge,
-                days: dateAge
-            };
-
-
-            return age;
-
-
-
-        },
-    },
-  
-}
+    return {
+      jobStartmonth: 6,
+      jobStartyear: 2019,
+      jobDuration: "",
+      aboutmeDescription: `I'm a Software Engineer from Bangladesh. I'm a true fan of competitive programming and web development. I love learning new technologies and playing with them`,
+    };
+  },
+};
 </script>
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,98 +1,107 @@
 <template>
-<div class="card border-0">
+  <div class="card border-0">
     <div class="card-header text-left darkblue-bg text-white">
-        {{title}}
-
+      {{ title }}
     </div>
     <div class="card-body text-left font-14">
-        <div>
-            <div class="card px-5 py-4 shadow">
-                <div class="row font-weight-bold card-title text-info  " style="font-size:16px;">Associate Software Engineer</div>
-                <div class="row card-text text-success">Syntec Consulting Limited </div>
-                <div class="row mt-1 mb-1 text-secondary"> Oct 2019 - Present</div>
-                <div class="ml-4">
-                    <div class="row font-italic ">Responsibilities:</div>
-                    <div class="row">
-                        <ul>
-                            <li>Developing new modules and modifying existing modules</li>
-                            <li>Colaborating with UI/UX and Senior developers</li>
-                      
-                        </ul>
-
-                    </div>
-                </div>
-                <div class="ml-4">
-                    <div class="row font-italic">
-                        Technologies:
-
-                    </div>
-                    <div class="row">
-                        <ul>
-
-                            <li>
-                                Front-end: Vue.js and AngularJS
-                            </li>
-                            <li>
-                                Back-end: C#, ASP.NET Web API, ASP.NET Core Web API, Entity Framework in db-first
-                                approach
-                            </li>
-                            <li>Database: Microsoft SQL Server, SQLite</li>
-                        </ul>
-                    </div>
-                </div>
+      <div>
+        <div
+          class="card px-5 py-4 mb-1 shadow"
+          v-for="(item, index) in experienceList"
+        >
+          <div
+            class="row font-weight-bold card-title text-info "
+            style="font-size:16px;"
+          >
+            {{ item.designation }}
+          </div>
+          <div class="row card-text text-success">
+            {{ item.companyName }}
+          </div>
+          <div class="row mt-1 mb-1 text-secondary">{{ item.duration }}</div>
+          <div class="ml-4">
+            <div class="row font-italic ">Responsibilities:</div>
+            <div class="row">
+              <ul>
+                <li v-for="(res, ind) in item.responsibilities">{{ res }}</li>
+              </ul>
             </div>
-             <div class="card px-5 py-4 shadow mt-5">
-                <div class="row font-weight-bold card-title text-info  " style="font-size:16px;">Software Engineering Intern</div>
-                <div class="row card-text text-success">Syntec Consulting Limited </div>
-                <div class="row mt-1 mb-1 text-secondary"> Jul 2019 - Sep 2019</div>
-                <div class="ml-4">
-                    <div class="row font-italic ">Responsibilities:</div>
-                    <div class="row">
-                        <ul>
-                            <li>Creating project as assignment to get adapted with the technologies</li>
-                            <li>Working in the main project</li>
-                         
-                      
-                        </ul>
-
-                    </div>
-                </div>
-                <div class="ml-4">
-                    <div class="row font-italic">
-                        Technologies:
-
-                    </div>
-                    <div class="row">
-                        <ul>
-
-                            <li>
-                                Front-end: AngularJS
-                            </li>
-                            <li>
-                                Back-end: C#, ASP.NET Web API, Entity Framework in db-first approach
-                            </li>
-                            <li>Database: Microsoft SQL Server</li>
-                        </ul>
-                    </div>
-                </div>
+          </div>
+          <div class="ml-4">
+            <div class="row font-italic">
+              Technologies:
             </div>
-           
-
+            <div class="row">
+              <ul>
+                <li v-for="(techValue, techName) in item.technologies">
+                  {{ techName }}: {{ techValue }}
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Experience',
-    props: {
-        title: String,
-
-    },
-   
-   
-
-}
+  name: "Experience",
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      experienceList: [
+        {
+          designation: "Associate Software Engineer",
+          companyName: "Kaz Software",
+          duration: "Feb 2021 - Present",
+          responsibilities: [
+            "Became an integral part of the team ",
+            "Worked in both frontend and backend on different technologies",
+            "Guided junior developers/interns to work on team projects and helped them perform different tasks",
+          ],
+          technologies: {
+            "Front-end": "React.js and Angular 10",
+            "Back-end":
+              "C#, ASP.NET Web API, ASP.NET Core Web API, Entity Framework in db-first approach",
+            Database: "Microsoft SQL Server, MySql",
+          },
+        },
+        {
+          designation: "Associate Software Engineer",
+          companyName: "Syntec Consulting Limited",
+          duration: "Oct 2019 - Jan2021",
+          responsibilities: [
+            "Developing new modules and modifying existing modules",
+            "Colaborating with UI/UX and Senior developers",
+          ],
+          technologies: {
+            "Front-end": "Vue.js and AngularJS",
+            "Back-end":
+              "C#, ASP.NET Web API, ASP.NET Core Web API, Entity Framework in db-first approach",
+            Database: "Microsoft SQL Server, SQLite",
+          },
+        },
+        {
+          designation: "Software Engineering Intern",
+          companyName: "Syntec Consulting Limited",
+          duration: "Jul 2019 - Sep 2019",
+          responsibilities: [
+            "Creating project as assignment to get adapted with the technologies",
+            "Working in the main project",
+          ],
+          technologies: {
+            "Front-end": "AngularJS",
+            "Back-end":
+              "C#, ASP.NET Web API, Entity Framework in db-first approach",
+            Database: "Microsoft SQL Server",
+          },
+        },
+      ],
+    };
+  },
+};
 </script>
